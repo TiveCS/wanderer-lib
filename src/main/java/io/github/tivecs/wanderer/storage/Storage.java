47de6c3d8497f50d545184e18bdaa2548b9d287a
TiveCS <creativecode.id@gmail.com>
2022-01-01 20:00:34 +0700

    Add documentation and quick action for StorageYML.java

--- src/main/java/io/github/tivecs/wanderer/storage/Storage.java
+++ src/main/java/io/github/tivecs/wanderer/storage/Storage.java
@@ -4,30 +4,98 @@ import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Base class for any storage type implementation.
+ */
 public abstract class Storage {
 
-    private String id;
+    private final String id;
 
-    private Map<Object, Object> data;
+    /**
+     * Act as temporary storage.
+     */
+    private final Map<Object, Object> data;
 
+    /**
+     * Act as changes storage for any changes on specific data path.
+     */
+    private final Map<Object, Object> dataChanges;
+
+    /**
+     * Create storage object for new source.
+     * @param id the storage source id.
+     */
     public Storage(String id){
         this.id = id;
         this.data = initializeDataMap();
+        this.dataChanges = initializeDataMap();
     }
 
+    /**
+     * Write data into changes storage.
+     * @param args settings for write data.
+     */
     public abstract void writeData(HashMap<Object, Object> args);
+
+    /**
+     * Delete data from changes storage.
+     * @param args settings for delete data.
+     */
     public abstract void deleteData(HashMap<Object, Object> args);
+
+    /**
+     * Get the data from temporary storage.
+     * @param args settings for reading data.
+     * @return data from temporary storage.
+     */
     public abstract Object readData(HashMap<Object, Object> args);
+
+    /**
+     * Load and get data from source, then store the data into temporary storage.
+     * @param overwrite will overwrite current temporary storage if true.
+     * @param args settings for load the data.
+     */
     public abstract void loadData(boolean overwrite, @Nullable HashMap<Object, Object> args);
+
+    /**
+     * Copy all data that registered on changes storage into temporary storage.
+     * @param args settings for save data.
+     */
     public abstract void saveData(@Nullable HashMap<Object, Object> args);
+
+    /**
+     * Save all data from current temporary storage into source.
+     * @param args settings for save data to source.
+     */
     public abstract void saveDataToSource(@Nullable HashMap<Object, Object> args);
 
+    /**
+     * Convert all data from temporary storage into wanted storage type.
+     * @param args settings for converting data.
+     * @return the converted temporary storage.
+     */
     public abstract Object convertData(HashMap<Object, Object> args); // TODO make conversion between other storage type
 
+    /**
+     * Set the Map type of temporary storage and changes storage.
+     * @return map type of storage
+     */
     public abstract Map<Object, Object> initializeDataMap();
 
+    /**
+     * Get the temporary storage.
+     * @return temporary storage
+     */
     public Map<Object, Object> getData() {
         return data;
+    }
+
+    /**
+     * Get the changes storage.
+     * @return changes storage
+     */
+    public Map<Object, Object> getDataChanges() {
+        return dataChanges;
     }
 
     public String getId() {
--- src/main/java/io/github/tivecs/wanderer/storage/StorageYML.java
+++ src/main/java/io/github/tivecs/wanderer/storage/StorageYML.java
@@ -6,6 +6,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -13,28 +14,64 @@ import java.util.concurrent.ConcurrentHashMap;
 public class StorageYML extends Storage{
 
     private File file = null;
-    private FileConfiguration config = null;
-    private Map<Object, Object> dataChanges;
+    private final FileConfiguration config;
 
+    /**
+     * Create new storage object with file name as storage id.
+     * @param file the source file
+     */
     public StorageYML(@Nonnull File file) {
         super(file.getName().split("[.]")[0]);
-        this.dataChanges = initializeDataMap();
         this.file = file;
         this.config = YamlConfiguration.loadConfiguration(file);
     }
 
+    /**
+     * Create new storage object with {@link FileConfiguration} as source. <br>
+     * Using FileConfiguration as source will be unable to save the data into file. <br>
+     * Recommended for storage that has behaviour as Read Only.
+     * @param id the storage source id
+     * @param configuration the storage source
+     */
     public StorageYML(String id, FileConfiguration configuration){
         super(id);
-        this.dataChanges = initializeDataMap();
         this.config = configuration;
+    }
+
+    /**
+     * Write data into changes storage.
+     * @param path the data path
+     * @param value the data value
+     * @param overwrite true will overwrite the data path if exists.
+     */
+    public void writeData(String path, Object value, boolean overwrite){
+        if (!overwrite && getData().containsKey(path)){
+            return;
+        }
+        getDataChanges().put(path, value);
     }
 
     @Override
     public void writeData(HashMap<Object, Object> args) {
+        boolean overwrite = true;
         String path = args.get("path").toString();
         Object value = args.get("value");
 
+        if (args.containsKey("overwrite")){
+            overwrite = (boolean) args.getOrDefault("overwrite", true);
+        }
+        if (!overwrite && getData().containsKey(path)){
+            return;
+        }
         getDataChanges().put(path, value);
+    }
+
+    /**
+     * Delete data from changes storage.
+     * @param path the data path
+     */
+    public void deleteData(String path){
+        getDataChanges().put(path, null);
     }
 
     @Override
@@ -42,6 +79,15 @@ public class StorageYML extends Storage{
         String path = args.get("path").toString();
 
         getDataChanges().put(path, null);
+    }
+
+    /**
+     * Load and get data from source, then store the data into temporary storage.<br>
+     * If already loaded before, all paths that do not exist on source file will not be replaced.
+     * @param overwrite true will clear current temporary storage then replace with newer one.
+     */
+    public void loadData(boolean overwrite){
+       loadData(overwrite, null);
     }
 
     @Override
@@ -55,15 +101,46 @@ public class StorageYML extends Storage{
         }
     }
 
+    /**
+     * Get the data from temporary storage.
+     * @param path the data path
+     * @return data from temporary storage
+     */
+    public Object readData(String path){
+        return getData().get(path);
+    }
+
     @Override
     public Object readData(HashMap<Object, Object> args) {
         return getData().get(args.get("path"));
+    }
+
+    /**
+     * Copy all data that registered on changes storage into temporary storage.
+     */
+    public void saveData(){
+        saveData(null);
     }
 
     @Override
     public void saveData(HashMap<Object, Object> args) {
         for (Map.Entry<Object, Object> entry : getDataChanges().entrySet()){
             getData().put(entry.getKey(), entry.getValue());
+        }
+    }
+
+    /**
+     * Save all data from current temporary storage into {@link FileConfiguration}.
+     * @param saveToFile true will save all data into source file directly.
+     */
+    public void saveDataToSource(boolean saveToFile){
+        saveDataToSource(null);
+        if (saveToFile && getFile() != null){
+            try {
+                getConfig().save(getFile());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -89,7 +166,7 @@ public class StorageYML extends Storage{
         return config;
     }
 
-    public Map<Object, Object> getDataChanges() {
-        return dataChanges;
+    public File getFile() {
+        return file;
     }
 }
