4fec9e887d85953133b4b7bbd9d90d12be8fa259
TiveCS <creativecode.id@gmail.com>
2022-01-10 18:27:19 +0700

    Add placeholder function for ItemStack and List

--- src/main/java/io/github/tivecs/wanderer/language/Placeholder.java
+++ src/main/java/io/github/tivecs/wanderer/language/Placeholder.java
@@ -1,9 +1,13 @@
 package io.github.tivecs.wanderer.language;
 
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
-public class Placeholder {
+public class Placeholder implements Cloneable {
 
     private final LinkedHashMap<String, String> placeholders = new LinkedHashMap<>();
 
@@ -17,6 +21,24 @@ public class Placeholder {
         getPlaceholders().remove(placeholder);
     }
 
+    public ItemStack useForItem(ItemStack item){
+        ItemMeta meta = item.getItemMeta();
+        if (meta != null) {
+            if (meta.hasDisplayName()) meta.setDisplayName(useLinear(meta.getDisplayName()));
+            if (meta.hasLore()) {
+                meta.setLore(useForList(meta.getLore()));
+            }
+        }
+        return item;
+    }
+
+    public List<String> useForList(List<String> list){
+        for (int i = 0; i < list.size(); i++){
+            list.set(i, useLinear(list.get(i)));
+        }
+        return list;
+    }
+
     public String useLinear(String text){
         for (Map.Entry<String, String> entry: getPlaceholders().entrySet()){
             StringBuilder path = new StringBuilder().append("%").append(entry.getKey()).append("%");
@@ -27,5 +49,10 @@ public class Placeholder {
 
     public LinkedHashMap<String, String> getPlaceholders() {
         return placeholders;
+    }
+
+    @Override
+    protected Placeholder clone() throws CloneNotSupportedException {
+        return (Placeholder) super.clone();
     }
 }
