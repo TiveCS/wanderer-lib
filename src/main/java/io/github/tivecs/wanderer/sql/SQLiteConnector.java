ec9d31e2a61c5fcee30b9b8c84726ca13715f7d2
TiveCS <creativecode.id@gmail.com>
2022-02-04 17:53:39 +0700

    Add getFile on SQLiteConnector

--- src/main/java/io/github/tivecs/wanderer/sql/SQLiteConnector.java
+++ src/main/java/io/github/tivecs/wanderer/sql/SQLiteConnector.java
@@ -2,6 +2,7 @@ package io.github.tivecs.wanderer.sql;
 
 import javax.annotation.Nonnull;
 import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,17 +10,30 @@ import java.sql.SQLException;
 public class SQLiteConnector extends SQLConnector {
 
     private Connection connection = null;
+    private File databaseFile = null;
 
     public SQLiteConnector(@Nonnull File databaseFile) {
         super("org.sqlite.JDBC", "jdbc:sqlite:" + databaseFile);
+        this.databaseFile = databaseFile;
     }
 
     public void setConnection(Connection connection) {
         this.connection = connection;
     }
 
+    public File getDatabaseFile() {
+        return databaseFile;
+    }
+
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
+        if (!getDatabaseFile().exists()){
+            try {
+                getDatabaseFile().createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         if (this.connection != null && !this.connection.isClosed()) return this.connection;
 
         Class.forName(getClassPath());
